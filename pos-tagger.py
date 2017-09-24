440e1a7e937639fa3323a1e2dc8f26bfed9bc77b
Nikolas Wangsaputra <13514048@std.stei.itb.ac.id>
2017-09-24 22:37:18 +0700

    update

--- pos-tagger.py
+++ pos-tagger.py
@@ -18,32 +18,14 @@ f = open('id-ud-train.conllu','r', encoding='utf-8')
 data = f.read()
 data = parse(data)
 
+def tp(i,j):
+    tes
+def sol(o,j):
+    tes
+
 def viterbi(N,T):
 	viterbi = {}
 	
-	
-def viterbi_segment(text, P):
-    """Find the best segmentation of the string of characters, given the
-    UnigramTextModel P."""
-    # best[i] = best probability for text[0:i]
-    # words[i] = best word ending at position i
-    n = len(text)
-    words = [''] + list(text)
-    best = [1.0] + [0.0] * n
-    ## Fill in the vectors best, words via dynamic programming
-    for i in range(n+1):
-        for j in range(0, i):
-            w = text[j:i]
-            if P[w] * best[i - len(w)] >= best[i]:
-                best[i] = P[w] * best[i - len(w)]
-                words[i] = w
-    ## Now recover the sequence of best words
-    sequence = []; i = len(words)-1
-    while i > 0:
-        sequence[0:0] = [words[i]]
-        i = i - len(words[i])
-    ## Return sequence of best words and overall probability
-    return sequence, best[-1]
 
 ## initiate model
 
@@ -75,12 +57,15 @@ for sentence in data:
     previous = 'START'
     contextDictionary[previous] += 1
     stc = [""]
+    labelv = [""]
     for word in sentence:
         stc.append(word['form'])
         if word['upostag'] != None:
             tag = word['upostag']
+            labelv.append(word['upostag'])
         else:
             tag = word['xpostag']
+            labelv.append(word['xpostag'])
         transitionDictionary[previous][tag] += 1
         contextDictionary[tag] += 1
         if emitDictionary.get(word['form'], None) == None:
@@ -92,7 +77,12 @@ for sentence in data:
         stc.remove('')
     except:
         pass
-    print(stc)
+    try:
+        labelv.remove('')
+    except:
+        pass
+    #print(stc)
+    #print(labelv)
 #for previous, tagDictionary in transitionDictionary.items():
 #    for tag, value in tagDictionary.items   ():
 #        print("PREVIOUS TAG: " + previous + "," + " TAG: " + tag + ". JUMLAH KEMUNCULAN : " + str(value))
